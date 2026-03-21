6687e9d842e237018e0ffe3bbe907a1a9802d70a
rodrixx1552 <rodrixx1552@users.noreply.github.com>
2026-03-20 22:43:03 -0300

    Delete tmp_check_msmc.js

--- tmp_check_msmc.js
+++
@@ -1,4 +0,0 @@
-const msmc = require('msmc');
-console.log('MSMC Keys:', Object.keys(msmc));
-if (msmc.Auth) console.log('msmc.Auth type:', typeof msmc.Auth);
-if (msmc.default) console.log('msmc.default Keys:', Object.keys(msmc.default));
